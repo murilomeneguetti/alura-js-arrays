82fc6a0903ee32b447b34c25d73cf83ebc06c44e
Murilo Meneguetti Pegoraro <murilomeneguetti@gmail.com>
2023-11-22 16:02:46 -0300

    primeiras aulas

---
+++ adicionando-nota.js
@@ -0,0 +1,9 @@
+const notas = [10, 6, 8];
+//adicionando nota no final
+notas.push(7);
+
+console.log(notas)
+
+const media = (notas[0] + notas[1] + notas[2] + notas[3]) / notas.length;
+
+console.log(media)
