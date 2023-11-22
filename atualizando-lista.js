7d4cfdf340d105dc9e0a19090133be1d332dd826
Murilo Meneguetti Pegoraro <murilomeneguetti@gmail.com>
2023-11-22 16:49:33 -0300

    mais exercicios

---
+++ atualizando-lista.js
@@ -0,0 +1,9 @@
+const nomes = ["João", "Ana", "Caio", "Lara", "Marjorie", "Leo"];
+
+//removendo ana e caio e coloca rodrigo no lugar
+nomes.splice(1, 2, 'Rodrigo');
+
+console.log(nomes)
+
+// nomes.push('Rodrigo')
+// console.log(nomes)
