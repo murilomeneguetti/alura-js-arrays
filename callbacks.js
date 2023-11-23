e3fadbd46db38aa6f488ccd920e4f2aa0906b152
Murilo Meneguetti Pegoraro <murilomeneguetti@gmail.com>
2023-11-23 13:19:51 -0300

    funções callback

---
+++ callbacks.js
@@ -0,0 +1,16 @@
+const nomes = ["Evaldo", "Mari", "Camis"];
+
+// nomes.forEach(function(nome) {
+//     console.log(nome);
+// })
+
+//usando arrow function
+nomes.forEach((nome) => {
+    console.log(nome);
+})
+
+function imprimeNome(nome) {
+    console.log(nome)
+}
+
+nomes.forEach(imprimeNome);
