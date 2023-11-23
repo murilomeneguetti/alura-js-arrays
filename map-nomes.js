495652d1820e981f396db5fea41e7cbaf698faf5
Murilo Meneguetti Pegoraro <murilomeneguetti@gmail.com>
2023-11-23 17:41:40 -0300

    mais exercicios

---
+++ map-nomes.js
@@ -0,0 +1,10 @@
+const nomes = ["ana Julia", "Caio vinicius", "BIA silva"];
+
+// const nomesMaiusculos = nomes.map((nome) => {
+//     return nome.toUpperCase();
+// })
+
+//escrevendo dessa forma, funciona igual a forma de cima, com o return e as chaves
+const nomesMaiusculos = nomes.map((nome) => nome.toUpperCase());
+
+console.log(nomesMaiusculos);
